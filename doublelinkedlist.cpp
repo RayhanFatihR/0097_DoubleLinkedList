61857d8dd65362fe33b14a8d1cc6d2e0062c7ceb
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-15 18:37:03 +0700

    Tahap 3

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -10,3 +10,20 @@ class Node
         Node * prev;
 };
 
+class DoubleLinkedList
+{
+    private:
+        Node * START;
+    
+    public:
+        DoubleLinkedList()
+        {
+            START = NULL;
+        }
+    void addNode()
+    {
+        int nim;
+        cout << "\nEnter the roll number of the student: ";
+        cin >> nim;
+    }
+}
