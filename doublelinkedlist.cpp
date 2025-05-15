df58ba07ee9318ec2999abf8a01a591fd2ea5719
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-15 19:11:28 +0700

    Tahap 17

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -97,5 +97,11 @@ class DoubleLinkedList
         // Step 1 : Traverse the list to find the node
         while (current != NULL && current->noMhs != rollNo)
             current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found" << endl;
+            return;
+        }
     }
 };
