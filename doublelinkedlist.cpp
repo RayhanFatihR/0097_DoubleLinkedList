13f648c8250d8e3de0a7e9a1753eb0c1e6804dc6
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-15 19:24:38 +0700

    Tahap 19

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -115,6 +115,10 @@ class DoubleLinkedList
         {
             // Step 3 : Link previous node to next of current
             current->prev->next = current->next; 
+
+            // Step 4 : if current is not the last node
+            if (current->next != NULL)
+                current->next->prev = current->prev; 
         }
     }
 };
