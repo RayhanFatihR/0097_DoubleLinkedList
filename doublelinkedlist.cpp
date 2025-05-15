a14478fc0568a0545fa7dc5e7aad7bd52e813a16
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-05-15 18:55:08 +0700

    Tahap 10

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -60,7 +60,13 @@ class DoubleLinkedList
         Node * current = START;
         while (current->next != NULL && current->next->noMhs < nim)
         {
-            
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll number not allowed" << endl;
+            return;
         }
     }
 }
